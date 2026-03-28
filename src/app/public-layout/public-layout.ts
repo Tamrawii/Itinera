20ac5b982680fc2d8f0daed9a68ab424664bd87a
Mohamed Chahine <chahinetamraoui2@gmail.com>
2026-03-28 20:23:21 +0100

    V3: Finished designing the landing page

--- src/app/public-layout/public-layout.ts
+++ src/app/public-layout/public-layout.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
+import { LineiconsComponent } from '@lineiconshq/angular-lineicons';
+import {
+  Buildings1Solid,
+  KnifeFork1Solid,
+  MapMarker5Solid,
+  Car2Solid,
+  Search1Outlined,
+  MapMarker5Outlined,
+  StarFatSolid,
+  HeartOutlined,
+  Bolt3Solid,
+} from '@lineiconshq/free-icons';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [Navbar],
+  imports: [Navbar, LineiconsComponent],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css',
 })
-export class PublicLayout {}
+export class PublicLayout {
+  Buildings1Solid = Buildings1Solid;
+  KnifeFork1Solid = KnifeFork1Solid;
+  MapMarker5Solid = MapMarker5Solid;
+  Car2Solid = Car2Solid;
+  Search1Outlined = Search1Outlined;
+  MapMarker5Outlined = MapMarker5Outlined;
+  StarFatSolid = StarFatSolid;
+  HeartOutlined = HeartOutlined;
+  Bolt3Solid = Bolt3Solid;
+
+  year = new Date().getFullYear();
+}
